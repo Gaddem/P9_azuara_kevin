64e58ecc19630ffaf0cc051021717b5893b0f9cb
lmk-kevin <kevin@lmksystem.fr>
2024-02-13 21:05:12 +0100

    Up

---
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -0,0 +1,59 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import {screen, waitFor} from "@testing-library/dom"
+import BillsUI from "../views/BillsUI.js"
+import Bills from  "../containers/Bills.js"
+import { bills } from "../fixtures/bills.js"
+import {ROUTES, ROUTES_PATH} from "../constants/routes.js";
+import {localStorageMock} from "../__mocks__/localStorage.js";
+import userEvent from '@testing-library/user-event'
+
+import router from "../app/Router.js";
+
+describe("Given I am connected as an employee", () => {
+  describe("", () => {
+    test("Then billWhen I am on Bills Page icon in vertical layout should be highlighted", async () => {
+
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      await waitFor(() => screen.getByTestId('icon-window'))
+      const windowIcon = screen.getByTestId('icon-window')
+      //to-do write expect expression
+
+    })
+    test("Then bills should be ordered from earliest to latest", () => {
+      document.body.innerHTML = BillsUI({ data: bills })
+      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+      const datesSorted = [...dates].sort(antiChrono)
+      expect(dates).toEqual(datesSorted)
+    })
+  })
+
+  describe('When I am on Bills Page and I click on the icon eye', () => {
+    test('A modal should open', async() => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      document.body.innerHTML = BillsUI({ data:[bills[0]] })
+      const store=null
+      new Bills({
+        document, onNavigate, store, localStorage: window.localStorage
+      })
+      const eye = screen.getByTestId('icon-eye')
+      userEvent.click(eye)
+      const modale = screen.getByTestId('modaleFile')
+      expect(modale).toBeTruthy()
+    })
+  })
+})
